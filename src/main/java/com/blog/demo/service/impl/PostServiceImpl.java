3bd2b060da7fd37a5bc3db4f74c74c15bb1ffd90
luke-h1 <luke.howsam@yahoo.com>
2022-06-27 23:19:32 +0100

    add pagination support

--- src/main/java/com/blog/demo/service/impl/PostServiceImpl.java
+++ src/main/java/com/blog/demo/service/impl/PostServiceImpl.java
@@ -5,6 +5,9 @@ import com.blog.demo.exception.ResourceNotFoundException;
 import com.blog.demo.payload.PostDto;
 import com.blog.demo.repository.PostRepository;
 import com.blog.demo.service.PostService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,9 +32,15 @@ public class PostServiceImpl implements PostService {
 
 
     @Override
-    public List<PostDto> getAllPosts() {
-        List<Post> posts = postRepository.findAll();
-        return posts.stream().map(post -> mapToDTO(post)).collect(Collectors.toList());
+    public List<PostDto> getAllPosts(int pageNo, int pageSize) {
+
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+
+        Page<Post> posts = postRepository.findAll(pageable);
+
+        List<Post> listOfPosts = posts.getContent();
+
+        return listOfPosts.stream().map(post -> mapToDTO(post)).collect(Collectors.toList());
     }
 
     @Override
